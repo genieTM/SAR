cb499a8f43cbfb07ccec6e6c336dc7c2465b6f68
genieTM <52430717+genieTM@users.noreply.github.com>
2020-04-28 20:52:49 +0900

    Update genie.js

--- genie.js
+++ genie.js
@@ -263,7 +263,7 @@ function showHideGenie() {
 	var _genie=document.getElementById('genie');
 	var _tglgenie=document.getElementById('tglGenie');
 	if(_genie.style.zIndex<0){
-		_tglgenie.style.backgroundColor="#efefff";
+		_tglgenie.style.backgroundColor="#fcfcfc";
 		_genie.style.backgroundColor="#efefff";
 		_genie.style.zIndex=101;
 		// _tglgenie.style.zIndex=101;
